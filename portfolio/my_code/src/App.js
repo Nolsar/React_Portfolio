00c3d941f6f988e62a812f7c0ecf6d99c31a9e2a
Sara Nolan <saraa.ann28@gmail.com>
2021-05-07 19:49:11 -0400

    added portfolio template code

---
+++ portfolio/my_code/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Navbar from "./components/navbar";
+// import Header from "./components/header";
+// import Footer from "./components/footer";
+// import Navbar from "./components/projects";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* <Header />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,155 @@
-import logo from './logo.svg';
-import './App.css';
+import Spinner from "react-bootstrap/Spinner";
+import "./App.css";
 
 function App() {
+  const mySites = [
+    {
+      title: "Project 1#",
+      gitHubLink: "https://github.com/ptums/project-1",
+      libraries: ["Bootstrap", "React", "Auth0"],
+      img: "https://mobirise.com/assets52/images/poster.png",
+      width: 300,
+      height: 200,
+    },
+    {
+      title: "Project 2#",
+      gitHubLink: "https://github.com/ptums/project-2",
+      libraries: ["Bootstrap", "React", "MongoDB"],
+      img:
+        "https://lh3.googleusercontent.com/QigP2YA5j4Um9Rq_-OF5SWip66Bg1Qa7U1jP_Ryr92g0ucmk4NVp-vH3E1hy65HLaTocCPjzc5MOC7KnYye5PlXI=w640-h400-e365-rj-sc0x00ffffff",
+      width: 300,
+      height: 200,
+    },
+    {
+      title: "Project 3#",
+      gitHubLink: "https://github.com/ptums/project-3",
+      libraries: ["Tailwind.css", "Next.js", "Prismic.io"],
+      img:
+        "https://previews.123rf.com/images/droidworks/droidworks1501/droidworks150101595/36174079-stylish-website-template-for-personal-portfolio.jpg",
+      width: 300,
+      height: 200,
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="jumbotron">
+        <>
+          <Spinner animation="border" variant="primary" />
+          <Spinner animation="border" variant="secondary" />
+          <Spinner animation="border" variant="success" />
+          <Spinner animation="border" variant="danger" />
+          <Spinner animation="border" variant="warning" />
+          <Spinner animation="border" variant="info" />
+          <Spinner animation="border" variant="light" />
+          <Spinner animation="border" variant="dark" />
+        </>
+        <h1>
+          {" "}
+          <Spinner animation="border" variant="dark" />
+          <strong>My Portfolio</strong>
+          <Spinner animation="border" variant="light" />
+        </h1>
+
+        <>
+          <Spinner animation="border" variant="primary" />
+          <Spinner animation="border" variant="secondary" />
+          <Spinner animation="border" variant="success" />
+          <Spinner animation="border" variant="danger" />
+          <Spinner animation="border" variant="warning" />
+          <Spinner animation="border" variant="info" />
+          <Spinner animation="border" variant="light" />
+          <Spinner animation="border" variant="dark" />
+        </>
       </header>
+      <div className="container">
+        <div className="row">
+          {mySites.map((site) => (
+            <div className="col-sm-12 col-md-4">
+              <div className="card">
+                <img
+                  src={site.img}
+                  className="card-img-top"
+                  alt={site.title}
+                  width={site.width}
+                  height={site.height}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{site.title}</h5>
+                  <p>Libraries I used</p>
+                  <ul className="list-unstyled">
+                    {site.libraries.map((lib) => (
+                      <li className="card-text">{lib}</li>
+                    ))}
+                  </ul>
+                  <a href={site.gitHubLink} className="btn btn-primary">
+                    Github Repo
+                  </a>
+                </div>
+              </div>
+              <div className="d-block w-100">
+                <Spinner animation="border" variant="primary" />
+                <Spinner animation="border" variant="secondary" />
+                <Spinner animation="border" variant="success" />
+                <Spinner animation="border" variant="danger" />
+                <Spinner animation="border" variant="warning" />
+                <Spinner animation="border" variant="info" />
+                <Spinner animation="border" variant="light" />
+                <Spinner animation="border" variant="dark" />
+              </div>
+              <div className="d-block w-100">
+                <Spinner animation="border" variant="primary" />
+                <Spinner animation="border" variant="secondary" />
+                <Spinner animation="border" variant="success" />
+                <Spinner animation="border" variant="danger" />
+                <Spinner animation="border" variant="warning" />
+                <Spinner animation="border" variant="info" />
+                <Spinner animation="border" variant="light" />
+                <Spinner animation="border" variant="dark" />
+              </div>
+              <div className="d-block w-100">
+                <Spinner animation="border" variant="primary" />
+                <Spinner animation="border" variant="secondary" />
+                <Spinner animation="border" variant="success" />
+                <Spinner animation="border" variant="danger" />
+                <Spinner animation="border" variant="warning" />
+                <Spinner animation="border" variant="info" />
+                <Spinner animation="border" variant="light" />
+                <Spinner animation="border" variant="dark" />
+              </div>
+              <div className="d-block w-100">
+                <Spinner animation="border" variant="primary" />
+                <Spinner animation="border" variant="secondary" />
+                <Spinner animation="border" variant="success" />
+                <Spinner animation="border" variant="danger" />
+                <Spinner animation="border" variant="warning" />
+                <Spinner animation="border" variant="info" />
+                <Spinner animation="border" variant="light" />
+                <Spinner animation="border" variant="dark" />
+              </div>
+              <div className="d-block w-100">
+                <Spinner animation="border" variant="primary" />
+                <Spinner animation="border" variant="secondary" />
+                <Spinner animation="border" variant="success" />
+                <Spinner animation="border" variant="danger" />
+                <Spinner animation="border" variant="warning" />
+                <Spinner animation="border" variant="info" />
+                <Spinner animation="border" variant="light" />
+                <Spinner animation="border" variant="dark" />
+              </div>
+              <div className="d-block w-100">
+                <Spinner animation="border" variant="primary" />
+                <Spinner animation="border" variant="secondary" />
+                <Spinner animation="border" variant="success" />
+                <Spinner animation="border" variant="danger" />
+                <Spinner animation="border" variant="warning" />
+                <Spinner animation="border" variant="info" />
+                <Spinner animation="border" variant="light" />
+                <Spinner animation="border" variant="dark" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
