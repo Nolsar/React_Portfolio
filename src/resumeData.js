8f7b485dd85060df4ce76752d05c6d5d3aa3d93e
Sara Nolan <saraa.ann28@gmail.com>
2021-05-14 19:59:23 -0400

    changed file structure

---
+++ src/resumeData.js
@@ -0,0 +1,120 @@
+let resumeData = {
+    // "imagebaseurl":"https://github.com/Nolsar/nolsar.github.io",
+    "name": "Sara Nolan",
+    "role": "Full-stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/sara-n-940845161/",
+    // "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/sara-n-940845161/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Nolsar",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"A future full-stack web developer, with a bachelor's degree from Rutgers University in Business Administration.f Information Technology and pursuing my B.Tech from here. I believe that to be successful in life, one needs to go after their dreams and passion, and never stop working towards them.",
+    "address":"Sara A. Nolan",
+    "website":"https://github.com/Nolsar",
+    "education":[
+      {
+        "UniversityName":"Rutgers University Business School",
+        "specialization":"Data Analytics",
+        "MonthOfPassing":"January",
+        "YearOfPassing":"2022",
+        "Achievements":"Dean's List"
+      },
+      {
+        "UniversityName":"Brookdale Community College",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2018",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Banfield Pet Hospital",
+        "specialization":"Practice Manager, Shift Lead",
+        "MonthOfLeaving":"Current",
+        "YearOfLeaving":"Current",
+        // "Achievements":"Some Achievements"
+      },
+      // {
+      //   "CompanyName":"Some Company",
+      //   "specialization":"Some specialization",
+      //   "MonthOfLeaving":"Jan",
+      //   "YearOfLeaving":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"NodeJS"
+      },
+      {
+        "skillname":"MySQL & R"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Extended Play",
+        "description":"application",
+        "imgurl":"images/portfolio/phone.jpg",
+        "deployedapp": "https://nolsar.github.io/extendedplay/",
+        "githuburl": "https://github.com/Nolsar/extendedplay"
+      },
+      {
+        "name":"Pet Speak Health",
+        "description":"web application for veterinarians",
+        "imgurl":"images/portfolio/project.jpg",
+        "deployedapp": "https://pet-speak-health.herokuapp.com/",
+        "githuburl": "https://github.com/Nolsar/Pet_Speak_Health"
+      },
+      {
+        "name":"Budget Tracker",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png",
+        "deployedapp": "https://shielded-wildwood-66211.herokuapp.com/",
+        "githuburl": "https://github.com/Nolsar/Online-Offline_Budget_Trackers"
+      },
+      {
+        "name":"Note Taker",
+        "description":"application",
+        "imgurl":"images/portfolio/phone.jpg",
+        "deployedapp": "https://nolsar-note-taker.herokuapp.com/",
+        "githuburl": "https://github.com/Nolsar/Note_Taker"
+      }
+    ],
+    // "testimonials":[
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   },
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   }
+    // ]
+  }
+  
+  export default resumeData
